93a4a6061ebf0a67b4e9fa0166ff034cfa053b0f
Kirill Taran <kirilltaran@hotmail.com>
2023-01-26 12:35:40 +0400

    Platform list updated

--- platforms.go
+++ platforms.go
@@ -1,4 +1,11 @@
 package platformcontracts
 
+const AmazonMusic = "amazon"
+const AppleMusic = "apple"
 const Deezer = "deezer"
+const Gaana = "gaana"
 const Spotify = "spotify"
+const Tidal = "tidal"
+const Uma = "uma"
+const YandexMusic = "yandex"
+const YouTubeMusic = "youtube"
